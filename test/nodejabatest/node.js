c07a31e97cfbcb10f539c32d8abbba017f3ac2d7
slemfkdlqm <slemfkdlqm@naver.com>
2023-07-27 16:22:02 +0900

    전반적 성능개선 및 리팩토링(#9)

---
+++ test/nodejabatest/node.js
@@ -0,0 +1,27 @@
+
+
+export function createCircle(x, y){
+    const video = document.getElementById("webcam");
+
+    let circle = document.createElement('div');
+    let circleFill = document.createElement('div');
+
+    circle.classList.add('node-circle');
+    circleFill.classList.add('node-circle-fill');
+
+    circle.appendChild(circleFill);
+    document.body.appendChild(circle);
+    
+    setGamePosition(video, circle, x, y, 60, 60);
+
+    return [circle,circleFill];
+}
+
+function setGamePosition(frame, el, x, y, w, h){
+    const rect = frame.getBoundingClientRect();
+    const newX = window.scrollX + rect.left + ((1.0 - x) * frame.videoWidth) - w/2;
+    const newY = window.scrollY + rect.top + (y * frame.videoHeight) - h/2;
+    el.style.left = newX + 'px';
+    el.style.top = newY + 'px';
+  
+  }
