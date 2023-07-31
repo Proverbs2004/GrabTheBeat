db809bac1cb9a2200c713af9ab2b97dc13f0c784
구기현 <rnrl4611@gmail.com>
2023-07-31 11:18:42 +0900

    init: Spring 프로젝트 생성에 대한 commit

---
+++ be/src/test/java/com/ygd/grab_the_beat/GrabTheBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygd.grab_the_beat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabTheBeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
