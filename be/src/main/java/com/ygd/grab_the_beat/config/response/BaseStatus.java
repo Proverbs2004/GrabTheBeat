953bddf3c802c4ebb881d9ea64cab7c60a501391
Lee-Jiseung <ljss98@naver.com>
2023-08-08 15:04:25 +0900

    feat: controller advice 구현 (#46)

--- be/src/main/java/com/ygd/grab_the_beat/config/response/BaseStatus.java
+++ be/src/main/java/com/ygd/grab_the_beat/config/response/BaseStatus.java
@@ -4,7 +4,11 @@ import lombok.Getter;
 
 @Getter
 public enum BaseStatus {
-    SUCCESS(true, "요청에 성공하셨습니다.");
+    SUCCESS(true, "요청에 성공하셨습니다."),
+
+    URI_NOT_EXIST(false, "존재하지 않는 URL입니다."),
+    METHOD_NOT_EXIST(false, "지원하지 않는 Method입니다."),
+    REQUEST_BODY_ERROR(false, "REQUEST BODY에 문제가 있습니다.");
 
     private final boolean isSuccess;
     private final String message;
