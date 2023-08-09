7b1aba64ec6f2fb82e6aa8f2001ae02ea72d7f62
dldntjr0516@gmail.com <dldntjr0516@gmail.com>
2023-08-09 18:00:48 +0900

    feat: 상대방 점수 실시간 통신 구현 완료 (#47)

--- test/websocketserver-test/index.js
+++ test/websocketserver-test/index.js
@@ -1,17 +1,19 @@
 import { createServer } from 'http';
 import { parse } from 'url';
 import { dispatchSocketServer } from './socketserver.js';
+import { dispatchChatServer } from './socketserver-chat.js';
+import { dispatchScoreServer } from './socketserver-score.js';
 
 // http 서버 생성
-const server = createServer();
+const chatServer = createServer();
 
-server.on('upgrade', function upgrade(request, socket, head) {
+chatServer.on('upgrade', function upgrade(request, socket, head) {
     const { pathname } = parse(request.url);
     // '/' 제거하여 code 추출
     const code = pathname.slice(1);
 
     // code와 일치하는 WebSocketServer
-    const wss = dispatchSocketServer(code);
+    const wss = dispatchChatServer(code);
     if (wss !== null && wss !== undefined) {
         wss.handleUpgrade(request, socket, head, function done(ws) {
             // connection 이벤트 수행
@@ -23,6 +25,32 @@ server.on('upgrade', function upgrade(request, socket, head) {
 });
 
 // 클라이언트 요청 준비
-server.listen(8000, () => {
+chatServer.listen(8000, () => {
     console.log(`Server Listening on port 8000`)
+})
+
+
+// http 서버 생성
+const scoreServer = createServer();
+
+scoreServer.on('upgrade', function upgrade(request, socket, head) {
+    const { pathname } = parse(request.url);
+    // '/' 제거하여 code 추출
+    const code = pathname.slice(1);
+
+    // code와 일치하는 WebSocketServer
+    const wss = dispatchScoreServer(code);
+    if (wss !== null && wss !== undefined) {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+            // connection 이벤트 수행
+            wss.emit('connection', ws, request);
+        })
+    } else {
+        socket.destroy();
+    }
+});
+
+// 클라이언트 요청 준비
+scoreServer.listen(8001, () => {
+    console.log(`Server Listening on port 8001`)
 })
