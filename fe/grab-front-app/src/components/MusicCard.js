db6064e952af8c07de549775da396db42aae5e46
slemfkdlqm <slemfkdlqm@naver.com>
2023-08-11 15:46:22 +0900

    Refactor: 싱글 게임 리액트에 합치기(#64)

--- fe/grab-front-app/src/components/MusicCard.js
+++ fe/grab-front-app/src/components/MusicCard.js
@@ -1,44 +1,123 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { FaAngleLeft, FaArrowRight } from 'react-icons/fa';
+// import { FaAngleLeft, FaArrowRight } from 'react-icons/fa';
 import { CgChevronDoubleLeft, CgChevronDoubleRight } from 'react-icons/cg';
 import './MusicCard.css';
 
 function MusicCard({ musicList, selectedMusic, handleMusicSelect }) {
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0); // Initialize with 0 or the appropriate value
+    const canPlayMusic = useRef(false);
 
+    // window.addEventListener('onbeforeactivate',()=>{
+    //     canPlayMusic.current=true;
+        
+    // });
+
+    // useEffect(()=>{
+    //     canPlayMusic.current=true;
+    // });
+
+    console.log(canPlayMusic.current);
 
     useEffect(() => {
-        if (selectedMusic) {
-            const audioElement = new Audio(selectedMusic.music_url);
-            audioElement.volume=0.4;
-            audioElement.play();
 
-            return () => {
-                audioElement.pause();
-            };
-        }
+        setTimeout(()=>{
+            if (selectedMusic) {
+                const audioElement = new Audio(selectedMusic.music_url);
+                
+                audioElement.volume=0.4;
+
+                console.log('music playing');
+                console.log(selectedMusic.music_url);
+                audioElement.play();
+    
+                return () => {
+                    audioElement.pause();
+                    console.log('music pause');
+                };
+            }
+        },500);
+
+
+            // if (selectedMusic) {
+            //     const audioElement = new Audio(selectedMusic.music_url);
+            //     console.log(audioElement);
+                
+            //     audioElement.volume=0;
+
+            //     setTimeout(() => {
+            //         console.log('music playing');
+            //         console.log(selectedMusic.music_url);
+            //         audioElement.volume=0.4;
+            //         audioElement.play();
+            //     }, 1000);
+    
+            //     return () => {
+            //         audioElement.pause();
+            //         console.log('music pause');
+            //     };
+            // }
+            
     }, [selectedMusic]);
 
-    const CustomPrevArrow = ({ onClick }) => (
-        <div className="slick-arrow custom-prev-arrow" onClick={() => {
-          handleMusicSelect(musicList[currentSlideIndex - 1]);
-          onClick();
-      }}>
-            <CgChevronDoubleLeft />
-        </div>
-    );
 
-    const CustomNextArrow = ({ onClick }) => (
-        <div className="slick-arrow custom-next-arrow" onClick={() => {
-            handleMusicSelect(musicList[currentSlideIndex + 1]);
-            onClick();
-        }}>
-            <CgChevronDoubleRight />
-        </div>
-    );
+    // const CustomPrevArrow = ({ onClick }) => (
+
+        
+
+    //     <div className="slick-arrow custom-prev-arrow" onClick={() => {
+    //       handleMusicSelect(musicList[currentSlideIndex - 1]);
+    //       onClick();
+    //   }}>
+    //         <CgChevronDoubleLeft />
+    //     </div>
+    // );
+    function CustomPrevArrow ({ onClick }){
+        let idx=musicList.length-1;
+        if(currentSlideIndex!==0)idx=currentSlideIndex-1;
+        // console.log(idx);
+        // console.log(musicList[idx]);
+
+        return(
+            <div className="slick-arrow custom-prev-arrow" onClick={() => {
+                handleMusicSelect(musicList[idx]);
+                onClick();}}>
+                <CgChevronDoubleLeft />
+            </div>
+        )
+    };
+
+
+
+
+
+    // const CustomNextArrow = ({ onClick }) => (
+    //     <div className="slick-arrow custom-next-arrow" onClick={() => {
+    //         handleMusicSelect(musicList[currentSlideIndex + 1]);
+    //         onClick();
+    //     }}>
+    //         <CgChevronDoubleRight />
+    //     </div>
+    // );
+    function CustomNextArrow ({ onClick }){
+        let idx=0;
+        if(currentSlideIndex!==musicList.length-1)idx=currentSlideIndex+1;
+
+        return(
+            <div className="slick-arrow custom-next-arrow" onClick={() => {
+                handleMusicSelect(musicList[idx]);
+                onClick();}}>
+                <CgChevronDoubleRight />
+            </div>
+        )
+    };
+
+
+
+
+
     try{
         console.log(selectedMusic.id);
 
