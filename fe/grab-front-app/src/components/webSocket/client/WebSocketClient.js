76d18df10e61b0306a9258d4adce0e3f1e270329
rnrlgus <rnrl4611@gmail.com>
2023-08-15 19:52:26 +0900

    chore: 비기능 코드 수정

--- fe/grab-front-app/src/components/webSocket/client/WebSocketClient.js
+++ fe/grab-front-app/src/components/webSocket/client/WebSocketClient.js
@@ -11,16 +11,12 @@ const Client = (props) => {
   const socket = useRef(null);
   const messageInputRef = useRef(null);
 
-  // const location = useLocation();
-  // const userName = new URLSearchParams(location.search).get('userName');
   console.log('props: ',props);
 const userName = props.userName;
   const roomId = props.sessionId;
   console.log('roomId : ', roomId)
 
   useEffect(() => {
-    // ws.current = new WebSocket('ws://localhost:8003','echo-protocol');
-    // console.log(ws.current);
 
     socket.current = io("ws://localhost:8000/chat?roomId=" + roomId, {
       reconnectionDelayMax: 10000,
@@ -29,23 +25,7 @@ const userName = props.userName;
 
 
 
-//   // 메세지 수신
-//   const receiveMessage = (event) => {
-//   const receivedMessage = JSON.parse(event.data);
-
-//   if (receivedMessage.fromServer) {
-//     // 서버에서 받은 메시지인 경우
-//     setChatHistory((prevChatHistory) => [...prevChatHistory, receivedMessage.content]);
-//     console.log("받았엉");
-//   } else {
-//     // 클라이언트에서 보낸 메시지인 경우
-//     setChatHistory((prevChatHistory) => [...prevChatHistory, `${receivedMessage.userName}: ${receivedMessage.message}`]);
-//   }
-//   scrollChatToBottom();
-// };
-
-// ws.current.onmessage = receiveMessage;
-          // 메시지 받았을 때
+    // 메시지 받았을 때
     socket.current.on('message', data => {
       const receivedMessage = JSON.parse(data);
       console.log('receiveMessage at socket.on', receivedMessage);
@@ -53,7 +33,6 @@ const userName = props.userName;
     })
 
 return () => {
-  // ws.current.close();
 };
 }, []);
 
@@ -62,18 +41,10 @@ const sendMessage = () => {
   if (message.trim() !== '') {
     const fullMessage = JSON.stringify({ userName, message });
     console.log(fullMessage);
-    // 웹 소켓의 상태가 "OPEN"일 때만 메시지를 보냄
-    // if (ws.current.readyState === WebSocket.OPEN) {
-    //   ws.current.send(fullMessage);
     socket.current.emit('message', fullMessage);
       setMessage('');
       console.log(fullMessage);
-      // 메시지를 채팅 로그에 추가하고 스크롤을 가장 아래로 내림
-      // setChatHistory((prevChatHistory) => [...prevChatHistory, `${userName}: ${message}`]);
       scrollChatToBottom();
-    // } else {
-    //   console.log("왜 안돼?");
-    // }
   }
 };
 
@@ -96,7 +67,6 @@ const sendMessage = () => {
       <div className="containersingleroom">
         <div className="chatbox">
           <div className='listandlog'style={{display:'flex'}}>
-        {/* <div className='list'>list</div> */}
           <div id="chat-log" className="chatlogbox" >
 
             {chatHistory.map((message, index) => (
@@ -116,7 +86,6 @@ const sendMessage = () => {
             onKeyUp={handleKeyPress}
             ref={messageInputRef}
           />
-          {/* <button onClick={sendMessage}>Send</button> */}
           </div>
 
       </div>
