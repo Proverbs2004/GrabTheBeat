54cfbdcb1394da3d3f5c3e3e6fe6d0b225a15ef9
Lee-Jiseung <ljss98@naver.com>
2023-08-16 14:15:24 +0900

    feat: 로그아웃 버튼 구현

--- fe/grab-front-app/src/pages/join/Join.js
+++ fe/grab-front-app/src/pages/join/Join.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import './Join.css';
 import { Link } from 'react-router-dom';
+import Header from 'pages/header/Header';
 
 function Join () {
     return (
         <div>
+            <div style={{ 
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '1rem'
+            }}>
             <ButtonBack />    
+            <Header />
+            </div>
             <div className="containerJoin">
                 <JoinTitle />
                 <div>
