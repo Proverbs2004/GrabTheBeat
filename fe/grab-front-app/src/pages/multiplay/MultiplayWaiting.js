00753b68e74345e0e04404c6ef4d891f135ad77f
Lee-Jiseung <ljss98@naver.com>
2023-08-17 17:09:53 +0900

    refactor: 랜덤 코드에서 소문자 제거 (#96)

--- fe/grab-front-app/src/pages/multiplay/MultiplayWaiting.js
+++ fe/grab-front-app/src/pages/multiplay/MultiplayWaiting.js
@@ -615,7 +615,7 @@ function resultGame(){
     };
 
     const generateRandomAlphaNumeric = (length) => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
         let result = '';
 
         for (let i = 0; i < length; i++) {
